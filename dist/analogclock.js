ebcda8ceee1735431e60f995e42ba6513b0141ac
Tomas Rudh <tomas.rudh@formpipe.com>
2025-06-21 12:18:00 +0200

    .

--- dist/analogclock.js
+++ dist/analogclock.js
@@ -671,7 +671,7 @@ class AnalogClock extends HTMLElement {
 customElements.define('analog-clock', AnalogClock);
 
 
-class AnalogClockEditor extends HTMLElement {
+class AnalogClockEditor extends LitElement {
   static get properties() {
     return {
       hass: {},
