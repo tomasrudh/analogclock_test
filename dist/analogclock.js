25b58f83329fb5366af2ae95db595576e0ae6625
Tomas Rudh <tomas.rudh@formpipe.com>
2025-06-22 10:15:59 +0200

    .

--- dist/analogclock.js
+++ dist/analogclock.js
@@ -710,12 +710,12 @@ render() {
       .hass=${this._hass}
       .data=${this._config}
       .schema=${[
-      { name: "locale", selector: { text: {} },
+      { name: "locale", selector: { text: {} }},
       { name: "timezone", selector: { text: {} }},
       { name: "show_timezone", selector: { boolean: null }},
       { name: "timezonedisplayname", selector: { text: {} }},
-      { name: "diameter", selector: { number: { min: 0, max: 1000 } } },
-      { name: "hide_weeknumber", selector: { boolean: null } }
+      { name: "diameter", selector: { number: { min: 0, max: 1000 } }},
+      { name: "hide_weeknumber", selector: { boolean: null }}
     ]}
       .computeLabel=${this._computeLabel}
       @value-changed=${this._valueChanged} 
