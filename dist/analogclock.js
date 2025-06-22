b5e5db92fed09b4ae97665f4ff8b6567f3eddddb
Tomas Rudh <tomas.rudh@formpipe.com>
2025-06-22 13:14:57 +0200

    .

--- dist/analogclock.js
+++ dist/analogclock.js
@@ -2,7 +2,7 @@ import {
   html, LitElement,
 } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 
-export class AnalogClock extends HTMLElement {
+class AnalogClock extends HTMLElement {
   static getConfigElement() {
     return document.createElement("analog-clock-editor");
   }
@@ -671,3 +671,67 @@ export class AnalogClock extends HTMLElement {
 }
 
 customElements.define('analog-clock', AnalogClock);
+
+
+class AnalogClockEditor extends LitElement {
+  static get properties() {
+    return {
+      hass: {},
+      _config: {},
+    };
+  }
+
+  setConfig(config) {
+    this._config = config;
+  }
+
+_valueChanged(ev) {
+  // We make a copy of the current config so we don't accidentally overwrite anything too early
+  var _config = Object.assign({}, this._config);
+  _config = ev.detail.value;
+
+  // And finally write back the updated configuration all at once
+  this._config = _config;
+
+  // A config-changed event will tell lovelace we have made changed to the configuration
+  // this make sure the changes are saved correctly later and will update the preview
+  const event = new CustomEvent("config-changed", {
+    detail: { config: _config },
+    bubbles: true,
+    composed: true,
+  });
+  this.dispatchEvent(event);
+}
+
+render() {
+  // console.info('render()');
+  return html`
+      <ha-form
+      .hass=${this._hass}
+      .data=${this._config}
+      .schema=${[
+      { name: "locale", selector: {} },
+      // { name: "timezone", selector: { text: {} }},
+      { name: "show_timezone", selector: { boolean: null }},
+      // { name: "timezonedisplayname", selector: { text: {} }},
+      { name: "diameter", selector: { number: { min: 0, max: 1000 } }},
+      { name: "hide_weeknumber", selector: { boolean: null }}
+    ]}
+      .computeLabel=${this._computeLabel}
+      @value-changed=${this._valueChanged} 
+      @focusout=${this.valueChanged}
+      ></ha-form>
+    `;
+}
+}
+
+customElements.define("analog-clock-editor", AnalogClockEditor);
+window.customCards = window.customCards || [];
+window.customCards.push({
+  type: "analog-clock",
+  name: "AnalogClock",
+  preview: true, // Optional - defaults to false
+  description: "An analog clock card", // Optional
+  documentationURL:
+    "https://github.com/tomasrudh/analogclock_test",
+});
